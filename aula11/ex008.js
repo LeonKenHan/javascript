45d1aece1f9fbd162ee750e0b182a9517ce9d41b
Leon <147669615+LeonKenHan@users.noreply.github.com>
2024-02-23 23:52:21 -0300

    a

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 12
+console.log('O console funcionou corretamente!')
+if (vel > 60) {
+    console.log('Você ultrapassou a velocidade permitida. MULTADO!')
+}
+console.log('Dirija sempre usando cinto de segurança!')
