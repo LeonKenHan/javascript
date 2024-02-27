8e010a3d847c83b15f2494c43fa420decd980d1c
Leon <147669615+LeonKenHan@users.noreply.github.com>
2024-02-26 23:03:32 -0300

    a

---
+++ aula12/ex011.js
@@ -0,0 +1,8 @@
+var idade = 22
+console.log('Você tem ${idade} anos.')
+    console.log('Não vota')
+} else if (idade < 18 || idade > 65) {
+    console.log('Voto opcional')
+} else {
+    console.log('Voto obrigatório')
+}
