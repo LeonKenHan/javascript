0e0a2fb3c472174f2ea79a62757eb800b44a4840
Leon <147669615+LeonKenHan@users.noreply.github.com>
2024-03-11 12:55:32 -0300

    a

---
+++ aula23/js/index.js
@@ -0,0 +1,7 @@
+var nome, sobrenome, NomeCompleto;
+
+nome = "Dimitri";
+sobrenome = "Teixeira";
+nomeCompleto = nome + " " + sobrenome;
+
+document.getElementById("texto").innerHTML = nomeCompleto;
