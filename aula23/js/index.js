34d798586a97389e0c4fbf97c9d6626131b01cc0
Leon <147669615+LeonKenHan@users.noreply.github.com>
2024-03-16 22:26:38 -0300

    a

--- aula23/js/index.js
+++ aula23/js/index.js
@@ -1,9 +1,19 @@
-function verificar() {
+var hora = new Date().getHours();
+alert(hora);
+
+if (hora <12) {
+    alert('Bom dia'); 
+} else if (hora < 18) {
+    alert('Boa tarde');
+} else {
+    alert('Boa noite');
+}
+
+function verificar(){
     let nome = document.getElementById("nome").value;
 
-
-    if (nome =="" || nome==null){
-        let p = document.getElementById("teste"):
+    if (nome =="" || nome==null) {
+        let p = document.getElementById("teste");
         p.innerHTML = "O campo não pode ser vazio";
         p.style.color = "red";
     }else{
