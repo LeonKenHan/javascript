55a92d9e78e6e7b2abf5d60914d924fdd9a1ea33
Leon <147669615+LeonKenHan@users.noreply.github.com>
2024-03-23 12:46:10 -0300

    a

--- aula27/js/index.js
+++ aula27/js/index.js
@@ -1,64 +1,26 @@
-let data = new Date();
+Function buscarCEP() {
+    let input = document.getElementById('cep').value;
 
-let ano = data.getFullYear();
-console.log(ano);
 
-let mes = data.getMonth();
+const ajax = new XMLHttpRequest();
+ajax.open('GET', 'https://viacep.com.br/ws/' + input + '/json/');
+ajax.send();
 
-const mesesDoAno = ["Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"]
-let mesEscrito = mesesdoAno[data.getMonth()];
+ajax.onload = function () {
+    //document.getElementById('texto').innerHTML = this.responseText;
+    
+    //TRANSFORMEI O TEXTO EM OBJETO
+    let obj = JSON.parse(this.responseText);
 
-let diaMes = data.getDate();
+    //AQUI PEGUEI OS VALORES QUE EU QUERIA
+    let logradouro = obj.logradouro;
+    let cidade = obj.localidade;
+    let estado = obj.uf;
 
-let diaSemana = data.getDay();
 
-const diasDaSemana = ["Domingo", "Segunda", "Terça", "Quarta", "Quinta", "Sexta", "Sábado"];
-let diaSemanaEscrito = diasDaSemana[data.getDay()];
-
-//PEGAR A HORA - DE 0 ATÉ 23
-let hora = data.getHours();
-
-//PEGAR A MINNUTOS - DE 0 ATÉ 59
-let minutos = data.getMinutes();
-
-//PEGAR SEGUNDOS - DE 0 ATÉ 59
-let segundos = data.getSeconds();
-console.log(segundos);
-
-//PEGAR MILISEGUNDOS - DE 0 ATÉ 999 
-let milisegundos = data.getMilliseconds();
-console.log(milisegundos);
-
-//PEGAR A DATA NO PADRÃO BRASILEIRO - DIA / MES / ANO
-let dataBR = data.toLocaleString('pt-BR', { dateStyle: 'short' });
-console.log(dataBR);
-
-//PEGAR OS VALORES SEPARADOS
-d = new Date();
-diMes = d.getDate();
-mes = d.getMonth() + 1;
-ano = d.getFullYear();
-
-function addZero(x) { return x< 10 ? '0' + x : '' +x; };
-
-let dataPadraoBR = addZero(diaMes) + "/" + addZero(mes) + "/" + ano;
-console.log(dataPadraoBR);
-
-//COMPARAR DATAS - MAIOR OU MENOR. EX: VENCIMENTOS 
-var hoje = new Date()
-var vencimento = new Date(2022, 0, 15);
-
-if(hoje > vencimento) {
-    console.log("Sua conta está vencida!");
-} else {
-    console.log("Ainda não venceu, tudo certo!");
+    document.getElementById('texto').innerHTML = "Logradouro:" +logradouro + "<br> Cidade: " + cidade + "<br> Estado:" + estado;
 }
 
-//DIFERENÇA ENTRE DUAS DATAS EM DIAS 
-var dataInicial= new Date();
-var dataFinal = new Date(2022, 11, 31);
-var diferencaTempo = dataFinal.getTime() - dataInicial.getTime();
 
-var diferencaDias = Math.ceil(diferencaTempo / (24 * 60 * 60 * 1000));
 
-console.log(diferencaDias + " dias");
+}
